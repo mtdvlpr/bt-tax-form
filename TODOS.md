1c156c2867c64b0f89cfcd84498c13e83386f5ad
Manoah Tervoort <149895ja@gmail.com>
2024-03-06 16:47:55 +0100

    first setup

---
+++ TODOS.md
@@ -0,0 +1,23 @@
+# Todos
+
+## HTML
+
+- [x] All inputs available
+- [x] Explanation when to skip certain inputs with anchor link.
+- [x] Validation via `required`, `pattern`, `minlength`, `min`, `max`
+- [ ] Explanation text for questions
+  
+## CSS
+
+- [x] Show/hide inputs if they are (un)necessary
+- [x] Validation via `:user-valid`, `:user-invalid`, `data-error-message`
+- [ ] Disable next link when form is invalid
+- [ ] Explanation text for questions
+
+## JavaScript
+
+- [x] Show/hide and enable/disable inputs if they are (un)necessary
+- [x] Validation via `onBlur`, `data-error-message` (if CSS fails)
+- [x] Date inputs with `min` and `max` of today's date via `data-min`, `data-max`
+- [ ] Disable next link when form is invalid
+- [ ] Explanation text for questions
---
+++ docs/assets/js/validation.js
@@ -0,0 +1,183 @@
+/**
+ * Map of controller names to their input elements
+ * @type {Object<string, {parent?: string; inputs: HTMLInputElement[]}>}
+ */
+const inputMap = {};
+
+/**
+ * Initializes the form validation
+ */
+function initValidation() {
+  const hiddenElements = document.querySelectorAll("[data-hide]");
+  const controllers = [];
+
+  // Fetch all controllers
+  hiddenElements.forEach((el) => {
+    const controller = el.getAttribute("data-hide");
+    if (!controllers.includes(controller)) {
+      controllers.push(controller);
+    }
+  });
+
+  controllers.forEach((controller) => {
+    // Fetch all inputs for each controller
+    const hiddenInputs = document.querySelectorAll(
+      `[data-hide="${controller}"] > input, [data-hide="${controller}"] :not([data-hide]) > input`
+    );
+    inputMap[controller] = {
+      parent: document
+        .querySelector(`[data-hide="${controller}"]`)
+        ?.parentElement?.closest(`[data-hide]`)
+        ?.getAttribute("data-hide"),
+      inputs: hiddenInputs,
+    };
+
+    const controllerEl = document.getElementById(controller);
+
+    // Toggle the section based on the initial state of the controller
+    toggleSection(controller, !controllerEl.checked);
+
+    // Add event listeners to each controller
+    const alternativeEl = document.getElementById(
+      `${controller.replace("-no", "-yes")}`
+    );
+
+    // No is checked, so hide the section
+    if (controllerEl) {
+      controllerEl.addEventListener("change", (e) => {
+        toggleSection(controller, !e.target.value);
+      });
+    }
+
+    // Yes is checked, so show the section
+    if (alternativeEl) {
+      alternativeEl.addEventListener("change", (e) => {
+        toggleSection(controller, !!e.target.value);
+      });
+    }
+  });
+}
+
+/**
+ * Toggles a section of inputs on or off
+ * @param {string} controller The controller name
+ * @param {boolean} show Whether to show or hide the section
+ */
+function toggleSection(controller, show = false) {
+  const containerEl = document.querySelector(`[data-hide=${controller}]`);
+  containerEl.style.display = show ? "" : "none";
+  const inputs = inputMap[controller].inputs;
+  if (inputs) {
+    inputs.forEach((input) => {
+      setDisabled(input, !show);
+      if (input.hasAttribute("data-required")) {
+        setRequired(input, show);
+      }
+    });
+  }
+
+  const children = Object.keys(inputMap).filter(
+    (key) => inputMap[key].parent === controller
+  );
+  children.forEach((child) => {
+    const childEl = document.getElementById(child);
+    toggleSection(child, show && !childEl.checked);
+  });
+}
+
+/**
+ * Sets the required attribute on the given input element
+ * @param {HTMLInputElement} el The input element to set the required attribute on
+ * @param {boolean} required Whether the input should be required or not
+ */
+function setRequired(el, required = false) {
+  if (el) {
+    el.toggleAttribute("required", required);
+    el.setAttribute("aria-required", required);
+  }
+}
+
+/**
+ * Sets the disabled attribute on the given input element
+ * @param {HTMLInputElement} el The input element to set the disabled attribute on
+ * @param {boolean} required Whether the input should be disabled or not
+ */
+function setDisabled(el, disabled = false) {
+  if (el) {
+    el.toggleAttribute("disabled", disabled);
+    el.toggleAttribute("aria-disabled", disabled);
+  }
+}
+
+initValidation();
+initErrorMessages();
+setNextDisabled();
+
+/**
+ * Sets the state of the next button based on form validity
+ */
+function setNextDisabled() {
+  return;
+  const form = document.querySelector("form");
+  const next = document.querySelector("footer nav span:nth-of-type(2) a");
+  const disable = !form.checkValidity();
+  setDisabled(next, disable);
+  next.href = disable ? "" : next.getAttribute("data-href");
+}
+
+/**
+ * Initializes error messages for form validation
+ */
+function initErrorMessages() {
+  const inputs = document.querySelectorAll("input");
+  inputs.forEach((input) => {
+    // Add blur event listener for all inputs
+    input.addEventListener("blur", (e) => {
+      setFeedbackMessage(e.target);
+      setNextDisabled();
+    });
+
+    if (input.getAttribute("type") === "date") {
+      // Add extra change event listener for date inputs
+      input.addEventListener("change", (e) => {
+        setFeedbackMessage(e.target);
+        setNextDisabled();
+      });
+
+      // Set the dynamic max attributes for date inputs
+      if (input.getAttribute("data-max") === "today") {
+        let today = new Date();
+        const offset = today.getTimezoneOffset();
+        today = new Date(today.getTime() - offset * 60 * 1000);
+        input.max = today.toISOString().split("T")[0];
+      }
+    }
+  });
+}
+
+/**
+ * Sets a feedback message on target element
+ * @param {HTMLInputElement} target The target element
+ */
+function setFeedbackMessage(target) {
+  const errorEl = document.getElementById(`${target.id}-desc`);
+  if (errorEl) {
+    const valid = target.validity.valid;
+    const cssMsg = window.getComputedStyle(errorEl, "::before").content;
+
+    // Set error message if CSS failed
+    if (cssMsg === "none" && valid) {
+      target.style.border = "";
+      errorEl.style.color = "green";
+      errorEl.innerText = "✓";
+    } else if (cssMsg === "none") {
+      target.style.border = "2px solid red";
+      errorEl.style.color = "red";
+      errorEl.innerText = errorEl.getAttribute("data-error-message");
+    } else {
+      target.style.border = "";
+      errorEl.style.color = "";
+      errorEl.innerText = "";
+    }
+  }
+}
